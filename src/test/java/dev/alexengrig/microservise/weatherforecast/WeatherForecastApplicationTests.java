0da49a6af452ca03d1b4cf427542436ee1727fa3
Grig Alex <alexengrigdev@gmail.com>
2021-07-04 00:24:01 +0300

    Initial commit

---
+++ src/test/java/dev/alexengrig/microservise/weatherforecast/WeatherForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.microservise.weatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
